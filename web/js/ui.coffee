8eaeb248fbab5e5503b1bf5e791b424110a04cd8
Lucas R. Martins <lukasrms@gmail.com>
2012-08-25 21:32:45 -0300

    shot button on main ui workink

--- web/js/ui.coffee
+++ web/js/ui.coffee
@@ -1,21 +1,50 @@
 class MainWindowView extends Backbone.View
     initialize: ->
         @$el.sortable(placeholder: "ui-state-highlight").disableSelection()
-
+        @$el.bind 'sortupdate', ->
+            window.camera_view.trigger('render')
+        
+class ToolbarView extends Backbone.View
+    events: 
+        'click button#shot': 'shot'
+    
+    initialize: ->
+        @$el.find('button').button()
+        
+    shot: ->
+        window.last_frame_view.trigger('render')
+        
 class CameraView extends Backbone.View
-    initialize: (options) ->
-        @opts = options
+    initialize: ->
+        @canvas = @$el.find('canvas')
+        @video = @$el.find('video')
+        @bind 'render', @render
         @render()
         
     render: ->
         navigator.webkitGetUserMedia 'video',
             (stream) =>
                 stream = window.webkitURL.createObjectURL stream
-                @$el.find('video').attr('src', stream)
+                @video.attr('src', stream)
            ,(exception) ->
                 console.log exception
+                
+class LastFrameView extends Backbone.View
+    initialize: (options) ->
+        @camera = window.camera_view
+        @bind 'render', @render
         
+    render: -> 
+        canvas = @camera.canvas[0]
+        video = @camera.video[0]
+        context = canvas.getContext '2d'
+        context.drawImage video, 0, 0
+        @$el.find('img').attr 'src', canvas.toDataURL 'image/webp'
+    
+              
 $(document).ready ->
-    main_window = new MainWindowView el: $ '#main-window'
-    main_window.camera_view = new CameraView {el: $('#camera-view'), main_window: main_window }
+    new MainWindowView el: $ '#main-window'
+    new ToolbarView el: $ '#toolbar-view'
+    window.camera_view = new CameraView el: $ '#camera-view'
+    window.last_frame_view = new LastFrameView el: $ '#last-frame-view'
     
--- web/js/ui.js
+++ web/js/ui.js
@@ -1,5 +1,5 @@
 (function() {
-  var CameraView, MainWindowView,
+  var CameraView, LastFrameView, MainWindowView, ToolbarView,
     __hasProp = Object.prototype.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
 
@@ -12,12 +12,39 @@
     }
 
     MainWindowView.prototype.initialize = function() {
-      return this.$el.sortable({
+      this.$el.sortable({
         placeholder: "ui-state-highlight"
       }).disableSelection();
+      return this.$el.bind('sortupdate', function() {
+        return window.camera_view.trigger('render');
+      });
     };
 
     return MainWindowView;
+
+  })(Backbone.View);
+
+  ToolbarView = (function(_super) {
+
+    __extends(ToolbarView, _super);
+
+    function ToolbarView() {
+      ToolbarView.__super__.constructor.apply(this, arguments);
+    }
+
+    ToolbarView.prototype.events = {
+      'click button#shot': 'shot'
+    };
+
+    ToolbarView.prototype.initialize = function() {
+      return this.$el.find('button').button();
+    };
+
+    ToolbarView.prototype.shot = function() {
+      return window.last_frame_view.trigger('render');
+    };
+
+    return ToolbarView;
 
   })(Backbone.View);
 
@@ -29,8 +56,10 @@
       CameraView.__super__.constructor.apply(this, arguments);
     }
 
-    CameraView.prototype.initialize = function(options) {
-      this.opts = options;
+    CameraView.prototype.initialize = function() {
+      this.canvas = this.$el.find('canvas');
+      this.video = this.$el.find('video');
+      this.bind('render', this.render);
       return this.render();
     };
 
@@ -38,7 +67,7 @@
       var _this = this;
       return navigator.webkitGetUserMedia('video', function(stream) {
         stream = window.webkitURL.createObjectURL(stream);
-        return _this.$el.find('video').attr('src', stream);
+        return _this.video.attr('src', stream);
       }, function(exception) {
         return console.log(exception);
       });
@@ -48,14 +77,44 @@
 
   })(Backbone.View);
 
+  LastFrameView = (function(_super) {
+
+    __extends(LastFrameView, _super);
+
+    function LastFrameView() {
+      LastFrameView.__super__.constructor.apply(this, arguments);
+    }
+
+    LastFrameView.prototype.initialize = function(options) {
+      this.camera = window.camera_view;
+      return this.bind('render', this.render);
+    };
+
+    LastFrameView.prototype.render = function() {
+      var canvas, context, video;
+      canvas = this.camera.canvas[0];
+      video = this.camera.video[0];
+      context = canvas.getContext('2d');
+      context.drawImage(video, 0, 0);
+      return this.$el.find('img').attr('src', canvas.toDataURL('image/webp'));
+    };
+
+    return LastFrameView;
+
+  })(Backbone.View);
+
   $(document).ready(function() {
-    var main_window;
-    main_window = new MainWindowView({
+    new MainWindowView({
       el: $('#main-window')
     });
-    return main_window.camera_view = new CameraView({
-      el: $('#camera-view'),
-      main_window: main_window
+    new ToolbarView({
+      el: $('#toolbar-view')
+    });
+    window.camera_view = new CameraView({
+      el: $('#camera-view')
+    });
+    return window.last_frame_view = new LastFrameView({
+      el: $('#last-frame-view')
     });
   });
 
