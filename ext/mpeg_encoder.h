b38b3bbe9baadda2f811606529fea74a7239164b
Gabriel Duarte <gabriel.costa@corp.globo.com>
2012-08-30 17:37:52 -0300

    few changes...

---
+++ ext/mpeg_encoder.h
@@ -0,0 +1,44 @@
+#ifndef mpeg_encoder_h
+#define mpeg_encoder_h
+//C headers
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+//C++ headers
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+
+//ffmpeg related headers
+#include "libavcodec/avcodec.h"
+#include "libavutil/mathematics.h"
+#include "libswscale/swscale.h"
+
+
+
+class RGB24MpegEncoder
+{
+    
+    int _w, _h;
+    int _size;
+    std::vector<uint8_t*> _frameList;
+    const char *_filename;
+    int _depth; 
+
+    public:
+        RGB24MpegEncoder(int , int);
+        RGB24MpegEncoder(uint8_t *, int , int);
+        ~RGB24MpegEncoder();
+        void addFrame(uint8_t *);
+	uint8_t *frameAt(int);
+        void setOutputFile(const char *outfile) { _filename = outfile; }
+	void encodeMpeg();
+	int frameListSize(){ return _frameList.size(); }
+	int width() { return _w; }
+	int heigth() { return _h; }
+};
+
+
+#endif //mpeg_encoder_h
