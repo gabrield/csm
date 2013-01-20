6a1979ea2a9c71c74a180fbb22de3490e398b3ab
Gabriel Duarte <gduarte.costa@globomail.com>
2013-01-20 17:55:56 -0200

    VideoEncoder alredy encoding

--- videoencoder.rb
+++ videoencoder.rb
@@ -1,18 +1,23 @@
+require 'open3'
+
 class VideoEncoder
 
 @_imglist
-@ffmpeg_cmd = "ffmpeg"
-@ffmpeg_params = "-y -f image2 -i img%d.png -sameq"
+@ffmpeg_cmd
+@ffmpeg_prms
 @outfile
 
   def initialize(outputfile)
-    @_imglist = Array.new
-    @outfile = outputfile
+    @_imglist    = Array.new
+    @outfile     = outputfile
+    @ffmpeg_cmd  = "ffmpeg"
+    @ffmpeg_prms = " -f image2 -i img%d.png -sameq -y " + @outfile
   end
 
   def encode()
     ffmpeg = Thread.new do
-      IO.popen([@_ffmpeg_cmd, @ffmpeg_params, @outfile])
+      p @ffmpeg_cmd, @ffmpeg_prms, @outfile
+      system @ffmpeg_cmd + @ffmpeg_prms
     end
     ffmpeg.join
   end
