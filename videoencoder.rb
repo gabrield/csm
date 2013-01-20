f035fa2bcae87fab46efe6e3b176c16f40f71340
Gabriel Duarte <gduarte.costa@globomail.com>
2013-01-20 16:23:30 -0200

    VideoEncoder list manipulation

--- videoencoder.rb
+++ videoencoder.rb
@@ -1,15 +1,25 @@
 class VideoEncoder
 
 @_imglist
-@ffmpeg_cmd
+@ffmpeg_cmd = "ffmpeg"
+@ffmpeg_params
+@outfile
 
-  def initilize(imglist, outputfile)
+  def initilize(outputfile)
+    @_imglist = Array.new
+    @outfile = outputfile
   end
 
   def encode()
+      
   end
 
   def addimg(img)
+    @_imglist.push img
+  end
+
+  def rmimg(img)
+    @_imglist.delete img
   end
 
 end
