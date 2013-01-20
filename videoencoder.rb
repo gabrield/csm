4a3f7f5aeaadcb41cee7282f6b890ef5a08d792a
Gabriel Duarte <gduarte.costa@globomail.com>
2013-01-20 18:02:34 -0200

    VideoEncoder cleanup

--- videoencoder.rb
+++ videoencoder.rb
@@ -2,13 +2,11 @@ require 'open3'
 
 class VideoEncoder
 
-@_imglist
 @ffmpeg_cmd
 @ffmpeg_prms
 @outfile
 
   def initialize(outputfile)
-    @_imglist    = Array.new
     @outfile     = outputfile
     @ffmpeg_cmd  = "ffmpeg"
     @ffmpeg_prms = " -f image2 -i img%d.png -sameq -y " + @outfile
