410d95664c448ee33994a74e0f1c4e91aa80513d
Gabriel Duarte <gduarte.costa@globomail.com>
2013-01-20 17:21:42 -0200

    VideoEncoder ffmpeg call added

--- videoencoder.rb
+++ videoencoder.rb
@@ -2,16 +2,19 @@ class VideoEncoder
 
 @_imglist
 @ffmpeg_cmd = "ffmpeg"
-@ffmpeg_params
+@ffmpeg_params = "-y -f image2 -i img%d.png -sameq"
 @outfile
 
-  def initilize(outputfile)
+  def initialize(outputfile)
     @_imglist = Array.new
     @outfile = outputfile
   end
 
   def encode()
-      
+    ffmpeg = Thread.new do
+      IO.popen([@_ffmpeg_cmd, @ffmpeg_params, @outfile])
+    end
+    ffmpeg.join
   end
 
   def addimg(img)
